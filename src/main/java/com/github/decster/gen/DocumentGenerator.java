30ed73dd280248f9fb5cfcb2c01c7bddee9ccfe5
Binglin Chang <decstery@gmail.com>
2025-06-03 18:59:08 +0800

    update

--- src/main/java/com/github/decster/gen/DocumentGenerator.java
+++ src/main/java/com/github/decster/gen/DocumentGenerator.java
@@ -1,4 +1,25 @@
 package com.github.decster.gen;
 
+import com.github.decster.ast.DocumentNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class DocumentGenerator {
+    DocumentNode documentNode;
+    String date;
+
+    public DocumentGenerator(DocumentNode documentNode, String date) {
+        this.documentNode = documentNode;
+        this.date = date;
+    }
+
+    /**
+     * Generates a map of generated file content, file path(relative to the output directory) -> file content as string
+     * @return Map<String, String> where the key is the file path and the value is the file content
+     */
+    public Map<String, String> generate() {
+        // TODO:
+        return new HashMap<String, String>();
+    }
 }
